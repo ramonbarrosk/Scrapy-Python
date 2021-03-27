c3c010d24fd9aaecb583de4f60e1be55e95e28c4
ramonbarrosk <ramonbarros6@gmail.com>
2021-03-27 03:11:29 -0300

    Alterando selenium

--- magalu/spiders/magalu.py
+++ magalu/spiders/magalu.py
@@ -7,9 +7,8 @@ class MagaluSpider(scrapy.Spider):
     name = "magazine"
     cep = "57100000"
     
-    
     def start_requests(self):
-        yield SeleniumRequest(url="https://www.magazineluiza.com.br/smartphone/celulares-e-smartphones/s/te/tcsp?page=1", callback=self.parse_celular)
+        yield SeleniumRequest(url="https://www.magazineluiza.com.br/smartphone/celulares-e-smartphones/s/te/tcsp?page=1", callback=self.parse_paginas)
 
     def parse_paginas(self,response):
         ultima_pagina = int(response.xpath('//li[@class="css-1a9p55p"][6]/a/text()').get())
@@ -25,17 +24,20 @@ class MagaluSpider(scrapy.Spider):
       
     def parse(self,response):
         driver = response.request.meta['driver']
-        cep = driver.find_element_by_name("zipcode")
-        botao = driver.find_element_by_xpath("//div[@class='freight-product__box']/div/button")
-        cep.send_keys("57100000")
-        driver.execute_script("arguments[0].click();",botao)
-        dados = driver.find_element_by_xpath('/html/body/div[3]/div[5]/div[1]/div[3]')
-        dados = dados.get_attribute('data-product')
-        dadosJson = json.loads(dados)
+        try:
+            cep  = driver.find_element_by_xpath('//input[@name="zipcode"]')
+            cep.send_keys("57100000")
+            botao = driver.find_element_by_xpath("//div[@class='freight-product__box']/div/button")
+            driver.execute_script("arguments[0].click();", botao)  
+        except Exception as e:
+            print(e)
+        dadosJson = response.xpath('//div[@class="header-product js-header-product"]/@data-product').get()
+        dadosJson = json.loads(dadosJson)
         nome = dadosJson.get('fullTitle')
         valor = dadosJson.get('bestPriceTemplate')
-        time.sleep(5)
+        time.sleep(15)
         frete = driver.find_element_by_xpath('//div[contains(@class,"js-freight-info freight-product__freight-text")]').text
+        
         yield {
             'nome_produto':nome,
             'valor_produto':valor,
